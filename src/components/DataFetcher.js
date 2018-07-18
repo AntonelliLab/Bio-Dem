aa784d241e6f87200d98a607bb087c807be1704f
Johannes Klein <johannes.t.klein@gmail.com>
2018-07-18 21:12:12 +0200

    Test component

---
+++ src/components/DataFetcher.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+/**
+ * A component that can be used for testing.
+ */
+class DataFetcher extends Component {
+  componentDidMount() {
+    console.log('DataFetcher');
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default DataFetcher;
