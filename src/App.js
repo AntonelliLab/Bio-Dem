af754c465e4a17285ea4a7ef19ce6fdb31b8cdc0
Daniel Edler <edler.dev@gmail.com>
2018-08-08 14:21:40 +0200

    Reduce space between plots by lazy mounting of warning components

--- src/App.js
+++ src/App.js
@@ -655,12 +655,12 @@ class App extends Component {
                       }))}
                     />
                   </FormControl>
-                  <Zoom in={xyYearIntervalLimited}>
+                  <Zoom in={xyYearIntervalLimited} mountOnEnter unmountOnExit>
                     <Notice variant="warning" message={
                       <span>Yearly data only available in the sub interval <strong>[{xyValidYears.toString()}]</strong> for the selected dimensions</span>
                     }/>
                   </Zoom>
-                  <Zoom in={gbifError['102']}>
+                  <Zoom in={gbifError['102']} mountOnEnter unmountOnExit>
                     <Notice variant="error" message={
                       <span>Error: Querying the GBIF API for country facet data failed</span>
                     }/>
@@ -746,12 +746,12 @@ class App extends Component {
                     }
                     label="Only show records with photo"
                   />
-                  <Zoom in={gbifError['101']}>
+                  <Zoom in={gbifError['101']} mountOnEnter unmountOnExit>
                     <Notice variant="error" message={
                       <span>Error: Querying the GBIF API for year facet data failed</span>
                     } />
                   </Zoom>
-                  <Zoom in={gbifError['103']}>
+                  <Zoom in={gbifError['103']} mountOnEnter unmountOnExit>
                     <Notice variant="error" message={
                       <span>Error: Querying the GBIF API for taxon data failed</span>
                     } />
